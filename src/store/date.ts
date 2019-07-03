fe7897e6de871d1e91f135dabde1ef39e0187f48
Daiji NISHIMURA <bundai223@gmail.com>
2019-07-03 20:57:45 +0900

    fix

--- src/store/date.ts
+++ src/store/date.ts
@@ -1,38 +1,38 @@
 import {
-  MutationAction,
-  Mutation,
-  Action,
-  VuexModule,
-  Module
+    MutationAction,
+    Mutation,
+    Action,
+    VuexModule,
+    Module
 } from 'vuex-module-decorators';
 
 export interface DateData {
-  public title: string = '';
-  public date: Date = new Date();
+    title: string;
+    date: Date;
 }
 
 @Module
 export default class Dates extends VuexModule {
-  private dates: DateData[] = [];
+    private dates: DateData[] = [];
 
-  get dateData() {
-    return this.dates;
-  }
+    get dateData() {
+        return this.dates;
+    }
 
-  @MutationAction({ mutate: ['dates'] })
-  public async newDate(title: string, date: Date) {
-    return {
-      dates: this.dates.push({})
-    };
-  }
+    @MutationAction({ mutate: ['dates'] })
+    public async newDate(title: string, date: Date) {
+        return {
+            dates: this.dates.push({ title: title, date: date })
+        };
+    }
 
-//   @Mutation
-//   public addDate(title: string, date: Date) {
-//     this.dates.push(new DateData());
-//   }
+    //   @Mutation
+    //   public addDate(title: string, date: Date) {
+    //     this.dates.push(new DateData());
+    //   }
 
-//   @Action({ commit: 'addDate' })
-//   public add() {
-//     return 5;
-//   }
-// }
+    //   @Action({ commit: 'addDate' })
+    //   public add() {
+    //     return 5;
+    //   }
+}
