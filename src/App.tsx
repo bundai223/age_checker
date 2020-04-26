ea1b6f62f0b7fe5a3b8b458d98834d456c1cd2cf
Daiji NISHIMURA <bundai223@gmail.com>
2020-04-27 01:39:20 +0900

    動くようになったもの

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      name: "hogehoge",
+    };
+  }
+  public render() {
+    return (
+      <div className="shopping-list">
+        <h1>Shopping List for </h1>
+        <ul>
+          <li>Instagram</li>
+          <li>Facebook</li>
+          <li>Mixi</li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
